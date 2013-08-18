0465eacf72f157f514177c054003658503b06766
Sam Sandberg <sam.sandberg@gmail.com>
2013-08-18 11:36:17 -0400

    Availability unit test for web

--- web/pushsi/web/tests.py
+++ web/pushsi/web/tests.py
@@ -1,16 +1,14 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class AvailabilityTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_web(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
