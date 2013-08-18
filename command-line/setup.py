b095d8dfd381c362f9752f732a3f21b422ab2ffc
Sam Sandberg <sam.sandberg@gmail.com>
2013-08-18 10:48:37 -0400

    Totally refactored command-line interface

--- command-line/setup.py
+++ command-line/setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 from distutils.core import setup
 
 setup(
     name='pushsi',
-    version='0.1',
-    description="Command line interface for sending push notifications to notify you of script results",
-    author="Sam Sandberg",
-    author_email="sam.sandberg@gmail.com",
-    scripts = ['pushsi', ],
+    version='0.2',
+    description="A simple library for sending push notifications to your phone, particularly via command line",
+    author="@loisaidasam",
+    scripts=['pushsi'],
+    requires=['requests'],
 )
